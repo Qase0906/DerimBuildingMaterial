c2f6af7c105e9b6df8ceb5dc6cece5a6a9055e93
Qase0906 <abdisamedabdi06@gmail.com>
2025-06-17 19:24:24 +0300

    add to cart functionality

--- main.js
+++ main.js
@@ -1,80 +1,113 @@
-const navbar = document.querySelector('.nav-bar');
-const bars = document.querySelector('.bars')
-const cart = document.querySelector('.cart')
-const cartPopup = document.querySelector('.cart-popup')
-const cancelCartPopup = cartPopup.querySelector('.times')
-const productContainer = document.querySelector('.product-container')
-
+const navbar = document.querySelector(".nav-bar");
+const bars = document.querySelector(".bars");
+const cart = document.querySelector(".cart");
+const cartPopup = document.querySelector(".cart-popup");
+const cancelCartPopup = cartPopup.querySelector(".times");
+const productContainer = document.querySelector(".product-container");
 
 // Responsive NavBar
-bars.addEventListener('click', (e)=> {
-    navbar.classList.toggle('active'); 
-    
-})
+bars.addEventListener("click", (e) => {
+  navbar.classList.toggle("active");
+});
 
 // Display Cart PopUP
-cart.addEventListener('click', ()=> {
-    cartPopup.classList.toggle('active-popup')
+cart.addEventListener("click", () => {
+  cartPopup.classList.toggle("active-popup");
 
-    if(cartPopup.classList.contains('active-popup')){
-        document.body.style.overflow = 'hidden';             
-    }else {
-        document.body.style.overflow = 'auto';
-    }
-    console.log(cartPopup.className)       
-    
-})
-
+//   if (cartPopup.classList.contains("active-popup")) {
+//     document.body.style.overflow = "hidden";
+//   } else {
+//     document.body.style.overflow = "auto";
+//   }
+ 
+});
 
 // JSON FETCH
 
 async function fetchData() {
-    
-    try {
+  try {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/Qase0906/DerimBuildingMaterial/main/products.json"
+    );
 
-        const response = await fetch('https://raw.githubusercontent.com/Qase0906/DerimBuildingMaterial/main/products.json')
+    const data = await response.json();
 
-        const data = await response.json()
-
-        // console.log(data[0])
-        displayProduct(data)
-        
-        
-    } catch (error) {
-        
-    }
-
+    // console.log(data[0])
+    displayProduct(data);
+  } catch (error) {}
 }
 
 fetchData();
 
-
 // Display Product on DOM
 
-function displayProduct(products){
-
-    products.forEach(product => {
-        
-        const productBox = document.createElement('div')
-        productBox.className = 'product-box';
-        productBox.innerHTML = `
-                    <img src="https://media.istockphoto.com/id/836219748/photo/set-of-construction-materials.jpg?b=1&s=612x612&w=0&k=20&c=bj52lUdeODGFK3bio4tEYLgRQ-2J1andzrH3HQxc6N8=" alt="">
+function displayProduct(products) {
+  products.forEach((product) => {
+    const productBox = document.createElement("div");
+    productBox.className = "product-box";
+    productBox.innerHTML = `
+                    <img src=${product.image} alt="">
                     <div class="product-content">
                         <p>${product.name}</p>
-                        <!-- <h2>Product Name</h2> -->
-                        <span><h2>BR200 - 1500</h2></span>
+                        <span><h2>BR${product.price}</h2></span>
                         <span>Min. order: 1 unit</span>
-                        
-                        <div class="btn" id="add-to-cart">
-                            <button>Add to Cart</button>
+                        <div class="btn" >
+                            <button class="add-to-cart">Add to Cart</button>
                         </div>
                     </div>
                 `;
-        productContainer.appendChild(productBox);
-        console.log(product.name);
+    productContainer.appendChild(productBox);    
+
+    addToCart(productBox, product);
+  });
+}
+
+// Add to cart
+
+let count = 1;
+
+function addToCart(productBox, product) {
+  const addCart = productBox.querySelector(".add-to-cart");
+     
+
+    addCart.addEventListener("click", () => {
+      const itemsContainer = cartPopup.querySelector(".items-container");
+      const itemsBox = document.createElement("div");
+      itemsBox.className = "items-box";
+
+      itemsBox.innerHTML = `
+                    <div class="img-title">
+                        <div class="pro-img">
+                            <img src=${product.image} alt="Wire">
+                        </div>
+                        <div class="pro-title">
+                            <h3>${product.name}</h3>
+                            <p>${product.category}</p>
+                        </div> 
+                    </div>
+
+                    <p id="price">${product.price}</p>
+                    
+                    <div class="num-items">
+                        <span>-</span>
+                        <span>0</span>
+                        <span>+</span>
+                    </div>
+                    <div class="delete">
+                        <span><i class="fa-solid fa-trash"></i></span>
+                    </div>
+            `;
+
+      itemsContainer.appendChild(itemsBox);
+      
+    //   Count Items in Cart
+      const countInCart = cart.querySelector('#cart-count');      
+      countInCart.textContent = count++;
+      
 
     });
 
-    
-
 }
+
+
+
