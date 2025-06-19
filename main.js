8b26c2776f97b6c21ee32926eb63b2f61179d733
Qase0906 <abdisamedabdi06@gmail.com>
2025-06-19 16:12:25 +0300

    enabled cart items to be read from localStorage

--- main.js
+++ main.js
@@ -4,6 +4,54 @@ const cart = document.querySelector(".cart");
 const cartPopup = document.querySelector(".cart-popup");
 const cancelCartPopup = cartPopup.querySelector(".times");
 const productContainer = document.querySelector(".product-container");
+const itemsContainer = cartPopup.querySelector(".items-container");
+const countInCart = cart.querySelector("#cart-count");
+const price = document.querySelector(".total-price h3");
+
+// Load Cart from LocalStorage
+
+window.addEventListener("DOMContentLoaded", loadToCart);
+
+function loadToCart() {
+  const getdata = JSON.parse(localStorage.getItem("items"));
+  if (!getdata) return;
+  const emptyCart = itemsContainer.querySelector(".empty-cart");
+  emptyCart.remove(); // if something is added to cart "Your cart is empty" text is removed.
+
+  getdata.forEach((product) => {
+    const itemsBox = document.createElement("div");
+    itemsBox.className = "items-box";
+
+    itemsBox.innerHTML = `
+                        <div class="img-title">
+                            <div class="pro-img">
+                                <img src=${product.image} alt="Wire">
+                            </div>
+                            <div class="pro-title">
+                                <h3>${product.name}</h3>
+                                <p>${product.category}</p>
+                            </div> 
+                        </div>
+
+                        <p id="price">${product.price}</p>
+                        
+                        <div class="num-items">
+                            <span>-</span>
+                            <span>0</span>
+                            <span>+</span>
+                        </div>
+                        <div class="delete">
+                            <span><i class="fa-solid fa-trash"></i></span>
+                        </div>
+                `;
+
+    itemsContainer.appendChild(itemsBox);
+    // console.log(itemsContainer);
+
+    price.textContent = totalAmount += product.price; // this line gives totalPrice of the product
+    countInCart.textContent = count++; // Total items you added to the Cart
+  });
+}
 
 // Responsive NavBar
 bars.addEventListener("click", (e) => {
@@ -13,17 +61,16 @@ bars.addEventListener("click", (e) => {
 // Display Cart PopUP
 cart.addEventListener("click", () => {
   cartPopup.classList.toggle("active-popup");
-
-//   if (cartPopup.classList.contains("active-popup")) {
-//     document.body.style.overflow = "hidden";
-//   } else {
-//     document.body.style.overflow = "auto";
-//   }
- 
 });
 
-// JSON FETCH
+window.addEventListener('click', (e)=> {
+  if(!cart.contains(e.target) && !cartPopup.contains(e.target)){
+    cartPopup.classList.remove('active-popup');
+  }
+  
+})
 
+// JSON FETCH API
 async function fetchData() {
   try {
     const response = await fetch(
@@ -34,14 +81,16 @@ async function fetchData() {
 
     // console.log(data[0])
     displayProduct(data);
+    // loadToCart(data)
   } catch (error) {}
 }
 
 fetchData();
 
 // Display Product on DOM
-
 function displayProduct(products) {
+  const productBoxes = [];
+
   products.forEach((product) => {
     const productBox = document.createElement("div");
     productBox.className = "product-box";
@@ -56,58 +105,99 @@ function displayProduct(products) {
                         </div>
                     </div>
                 `;
-    productContainer.appendChild(productBox);    
-
-    addToCart(productBox, product);
+    productContainer.appendChild(productBox);
+    productBoxes.push(productBox);
   });
+  addToCart(products, productBoxes);
 }
 
 // Add to cart
-
 let count = 1;
+let totalAmount = 0;
 
-function addToCart(productBox, product) {
-  const addCart = productBox.querySelector(".add-to-cart");
-     
+function addToCart(products, productBoxes) {
+  products.forEach((product, index) => {
+    const productBox = productBoxes[index];
+    const addCart = productBox.querySelector(".add-to-cart");
+    const emptyCart = itemsContainer.querySelector(".empty-cart");
 
     addCart.addEventListener("click", () => {
-      const itemsContainer = cartPopup.querySelector(".items-container");
       const itemsBox = document.createElement("div");
       itemsBox.className = "items-box";
+      itemsBox.setAttribute("data-id", product.id);
+
+      // Checks if the product in cart
+      const getData = JSON.parse(localStorage.getItem("items"));
+      const existingItemInCart = itemsContainer.querySelector(
+        `[data-id="${product.id}"]`
+      );
+      for (const key in getData) {
+        if (getData[key].id === product.id) {
+          alert("Item already in cart!");
+          return;
+        } else if (existingItemInCart) {
+          alert("Item already in cart!");
+          return;
+        }
+      }
+
+      if (emptyCart) {
+        emptyCart.remove(); // if something is added to cart "Your cart is empty" text is removed.
+      }
 
       itemsBox.innerHTML = `
-                    <div class="img-title">
-                        <div class="pro-img">
-                            <img src=${product.image} alt="Wire">
-                        </div>
-                        <div class="pro-title">
-                            <h3>${product.name}</h3>
-                            <p>${product.category}</p>
-                        </div> 
-                    </div>
+                          <div class="img-title">
+                              <div class="pro-img">
+                                  <img src=${product.image} alt="Wire">
+                              </div>
+                              <div class="pro-title">
+                                  <h3>${product.name}</h3>
+                                  <p>${product.category}</p>
+                              </div> 
+                          </div>
 
-                    <p id="price">${product.price}</p>
-                    
-                    <div class="num-items">
-                        <span>-</span>
-                        <span>0</span>
-                        <span>+</span>
-                    </div>
-                    <div class="delete">
-                        <span><i class="fa-solid fa-trash"></i></span>
-                    </div>
-            `;
+                          <p id="price">${product.price}</p>
+                          
+                          <div class="num-items">
+                              <span>-</span>
+                              <span>0</span>
+                              <span>+</span>
+                          </div>
+                          <div class="delete">
+                              <span><i class="fa-solid fa-trash"></i></span>
+                          </div>
+                  `;
 
       itemsContainer.appendChild(itemsBox);
-      
-    //   Count Items in Cart
-      const countInCart = cart.querySelector('#cart-count');      
+
+      // Total Price
+      price.textContent = totalAmount += product.price;
+
+      //   Count Items in Cart
       countInCart.textContent = count++;
-      
 
+      const items = {
+        id: product.id,
+        name: product.name,
+        image: product.image,
+        price: product.price,
+        category: product.category,
+      };
+
+      setItemstoLocalStorage(items);
+      // console.log(itemsBox)
     });
-
+  });
 }
 
+// set To localStorage
+function setItemstoLocalStorage(item) {
+  const storedItems = JSON.parse(localStorage.getItem("items")) || [];
 
+  const existItem = storedItems.find((stored) => stored.id == item.id);
 
+  if (!existItem) {
+    storedItems.push(item);
+    localStorage.setItem("items", JSON.stringify(storedItems));
+  }
+}
